2cd18af0185f3980ff16b2aacf47b159e35a18bc
Mr-Wusu <prince.agboinou_wusu@yahoo.com>
2025-08-14 15:37:24 +0100

    Just a little padding style implemented

--- app/_components/TrustedBy.tsx
+++ app/_components/TrustedBy.tsx
@@ -3,7 +3,7 @@ import ContinuousSlider from "./ContinuousSlider";
 export default function TrustedBy() {
   return (
     <section className="flex flex-col gap-3 bg-white pt-10  overflow-hidden">
-      <h2 className="text-center text-xl font-semibold">
+      <h2 className="text-xl pl-6 font-semibold w-3/4">
         Trusted by leading organizations worldwide
       </h2>
       <ContinuousSlider />
