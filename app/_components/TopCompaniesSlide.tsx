44dac3c7de77de870bce98d6cae9604def690d1f
Mr-Wusu <prince.agboinou_wusu@yahoo.com>
2025-08-19 16:48:30 +0100

    Last slide still with some problems

--- app/_components/TopCompaniesSlide.tsx
+++ app/_components/TopCompaniesSlide.tsx
@@ -1,19 +1,49 @@
 "use client";
 import topCompanies from "@/lib/top-companies";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/pagination";
 import TopCompaniesCard from "./TopCompaniesCard";
 
 export default function TopCompaniesSlide() {
   return (
-    <div className="ml-5">
-      <Swiper spaceBetween={-60} slidesPerView={1.2} className="ml-5">
+    <div className="ml-5 relative">
+      <Swiper
+        spaceBetween={-60}
+        slidesPerView={1.2}
+        centeredSlides={true} // Add this line
+        className="ml-5 pb-16"
+        modules={[Pagination]}
+        pagination={{ clickable: true }}
+      >
         {topCompanies.map((company, i) => (
           <SwiperSlide key={`Company ${i + 1} `} className="!w-95">
             <TopCompaniesCard card={company} />
           </SwiperSlide>
         ))}
       </Swiper>
+      <style jsx global>
+        {`
+          .swiper-pagination-bullet {
+            background-color: #999;
+            opacity: 1;
+            width: 12px;
+            height: 12px;
+            margin: 0 6px;
+          }
+
+          .swiper-pagination-bullet-active {
+            background-color: #172554;
+          }
+
+          .swiper-pagination {
+            position: static !important;
+            margin-top: 1rem !important;
+            text-align: center !important;
+          }
+        `}
+      </style>
     </div>
   );
 }
